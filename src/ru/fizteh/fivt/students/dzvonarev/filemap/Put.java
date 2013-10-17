b5fc16f4e39ea6948220762528923787d3330054
Dmitry Zvonarev <dmitry.zvonorev@phystech.edu>
2013-10-17 13:17:48 +0400

    2 version of FileMap

--- src/ru/fizteh/fivt/students/dzvonarev/filemap/Put.java
+++ src/ru/fizteh/fivt/students/dzvonarev/filemap/Put.java
@@ -7,10 +7,14 @@ public class Put {
 
     public static void putItem(String str, int spaceIndex) throws IOException {
         int newSpaceIndex = str.indexOf(' ', spaceIndex + 1);
-        if (str.indexOf(' ', newSpaceIndex + 1) != -1) {
+        if (newSpaceIndex == -1) {
             throw new IOException("put: wrong input");
         }
-        String key = str.substring(spaceIndex + 1, newSpaceIndex);
+        int index = newSpaceIndex;
+        while (str.indexOf(' ', newSpaceIndex + 1) == newSpaceIndex + 1) {
+            ++newSpaceIndex;
+        }
+        String key = str.substring(spaceIndex + 1, index);
         String value = str.substring(newSpaceIndex + 1, str.length());
         HashMap<String, String> fileMap = DoCommand.getFileMap();
         if (fileMap.containsKey(key)) {
